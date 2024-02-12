5d6bcbd5e19714adfc18bb3928c07586ee5a9544
Type-Here <94553428+Type-Here@users.noreply.github.com>
2024-02-12 02:03:47 +0100

    fixed low values

--- main/__init__.py
+++ main/__init__.py
@@ -4,6 +4,9 @@ import sys
 import subprocess
 
 
+# For KDE and dbus
+
+
 def get_backlight():
     return float(subprocess.check_output(["qdbus",
                                           "org.kde.Solid.PowerManagement",
@@ -13,28 +16,31 @@ def get_backlight():
 
 def set_backlight(backlight):
     value = "int32:%d" % backlight
-    subprocess.run(["dbus-send", "--print-reply", "--gitgit dest=org.kde.Solid.PowerManagement",
+    subprocess.run(["dbus-send", "--print-reply", "--dest=org.kde.Solid.PowerManagement",
                     "/org/kde/Solid/PowerManagement/Actions/BrightnessControl",
                     "org.kde.Solid.PowerManagement.Actions.BrightnessControl.setBrightness",
                     value])
 
 
 def calculate_step(backlight: float, log_min: float, log_max: float, tot_steps: int) -> int:
+    if backlight <= 1:
+        return round(backlight)
     return round(log10(backlight) / (log_max - log_min) * tot_steps)
 
 
 def get_new_backlight(cur_backlight: float, min_backlight: int,
                       max_backlight: int, tot_steps: int, increase=True) -> int:
-
     x_min = 0 if min_backlight <= 1 else log10(min_backlight)
     x_max = log10(max_backlight)
 
     current_step = calculate_step(cur_backlight, x_min, x_max, tot_steps)
     new_step = current_step + 1 if increase else current_step - 1
-    print("Values: ", new_step, min_backlight, max_backlight, tot_steps)
+    print("Values: ", new_step, current_step, min_backlight, max_backlight, tot_steps)
 
     if new_step == 0:
         return min_backlight
+    elif new_step == 1 or new_step == 2:
+        return 1
 
     x = new_step / tot_steps * (x_max - x_min)
     backlight = round(max(min(10 ** x, max_backlight), min_backlight))
@@ -57,8 +63,12 @@ if __name__ == "__main__":
 
     action = sys.argv[1]
     if action == "-i":
+        if current_backlight == backlight_max:
+            exit(0)
         new_backlight = get_new_backlight(current_backlight, backlight_min, backlight_max, steps, True)
     elif action == "-d":
+        if current_backlight == 0 or current_backlight == backlight_min:
+            sys.exit(0)
         new_backlight = get_new_backlight(current_backlight, backlight_min, backlight_max, steps, False)
     else:
         print("usage:\n\t{0} -i / -d".format(sys.argv[0]))
