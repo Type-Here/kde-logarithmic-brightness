5b740b849f05dc0538ea99d0872d00a34e19248b
Type-Here <94553428+Type-Here@users.noreply.github.com>
2025-01-18 19:10:20 +0100

    python full plasma 6 compatibility off.sh: use 6 version of qdbus if -6 argv is present

--- src/__init__.py
+++ src/__init__.py
@@ -10,17 +10,16 @@ Use of dbus instead of directly use /sys/class/backlight/<dev_name> node
 because dbus doesn't need sudo privileges
 """
 
-
-def get_max_brightness():
-    return int(subprocess.check_output(["qdbus",
+def get_max_brightness(QDBUS = "qdbus"):
+    return int(subprocess.check_output([QDBUS,
                                         "org.kde.Solid.PowerManagement",
                                         "/org/kde/Solid/PowerManagement/Actions/BrightnessControl",
                                         "org.kde.Solid.PowerManagement.Actions.BrightnessControl.brightnessMax"]))
 
 
-def get_backlight():
+def get_backlight(QDBUS = "qdbus"):
     """Get brightness value with qdbus"""
-    return float(subprocess.check_output(["qdbus",
+    return float(subprocess.check_output([QDBUS,
                                           "org.kde.Solid.PowerManagement",
                                           "/org/kde/Solid/PowerManagement/Actions/BrightnessControl",
                                           "org.kde.Solid.PowerManagement.Actions.BrightnessControl.brightness"]))
@@ -37,18 +36,28 @@ def set_backlight(backlight):
 
 if __name__ == "__main__":
     backlight_min = 0
-    backlight_max = get_max_brightness()
     steps = 20
 
-    if len(sys.argv) < 2 or sys.argv[1] not in ["-i", "-d"]:
+    if len(sys.argv) < 2 or not any(arg in ["-i", "-d"] for arg in sys.argv):
         print("usage:\n\t{0} -i / -d [method]".format(sys.argv[0]))
         print("method: optional; values: exp or log")
         sys.exit(0)
+    
+    if "-6" in sys.argv:
+        QDBUS = "qdbus6"
+        print("Using qdbus6")
+    else:
+        QDBUS = "qdbus"
+        print("Using qdbus")
+
+    # Get max brightness value
+    backlight_max = get_max_brightness(QDBUS=QDBUS)
+
 
     method = sys.argv[2] if len(sys.argv) == 3 else None
     Method = LogarithmicCalc if method == "log" else ExponentialCalc
 
-    current_backlight = get_backlight()
+    current_backlight = get_backlight(QDBUS=QDBUS)
 
     action = sys.argv[1]
     if action == "-i":
@@ -61,7 +70,7 @@ if __name__ == "__main__":
         new_backlight = Method.get_new_backlight(current_backlight, backlight_min, backlight_max, steps, increase=False)
     else:
         print("usage:\n\t{0} -i / -d".format(sys.argv[0]))
-        sys.exit(0)
+        sys.exit(1)
 
-    print("Current backlight: {0}\nChanging to: {1}".format(current_backlight, new_backlight))
+    #print("Current backlight: {0}\nChanging to: {1}".format(current_backlight, new_backlight))
     set_backlight(new_backlight)
